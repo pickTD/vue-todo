9b32d7bbed7da385415221bed7e36d3a300bdce8
picktd <vladislav.panferov@gmail.com>
2020-02-16 20:03:01 +0700

    routes change

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,25 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '@/views/Home.vue';
+import Edit from '@/views/Edit.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/:id',
+    name: 'edit',
+    component: Edit,
   },
 ];
 
 const router = new VueRouter({
+  mode: 'history',
   routes,
 });
 
